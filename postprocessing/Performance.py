0fbaa1788384e8c3348719ad4fe87709d1a5ee45
Michael Staneker <50531288+MichaelSt98@users.noreply.github.com>
2021-12-08 22:17:06 +0100

    Updated Performance postprocessing script

--- postprocessing/Performance.py
+++ postprocessing/Performance.py
@@ -25,6 +25,9 @@ class Performance:
     def get_data_average(self):
         return [mean(elem) for elem in self.data]
 
+    def get_data_max(self):
+        return [max(elem) for elem in self.data]
+
     def average(self):
         averages = []
         for proc in range(self.numProcesses):
@@ -45,8 +48,16 @@ if __name__ == '__main__':
             print(key)
             name = key.replace("time/", "")
             performance[key] = Performance(name, f[key][:])
+            if key == "time/rhsElapsed":
+                print(performance[key].get_data_average())
 
     f.close()
+
+    rhsElapsedPerStep = performance["time/rhsElapsed"].get_data_max()
+    print("rhs elapsed per step: {}".format(rhsElapsedPerStep))
+    fig, ax1 = plt.subplots(nrows=1, sharex=True)
+    ax1.plot(np.arange(0, len(rhsElapsedPerStep)), rhsElapsedPerStep)
+    plt.show()
 
     for key, value in performance.items():
         print("{}: {}".format(key, value.average()))
@@ -58,30 +69,51 @@ if __name__ == '__main__':
     #relevantKeys = ["time/loadBalancing", "time/reset", "time/boundingBox", "time/assignParticles", "time/tree",
     #                "time/pseudoParticle ", "time/gravity"]  # , "time/sph"]
 
-    relevantKeys = ["time/tree", "time/gravity", "time/reset", "time/boundingBox", "time/assignParticles",
-                    "time/pseudoParticle", "time/loadBalancing"]
+    # relevantKeys = ["time/tree", "time/gravity", "time/reset", "time/boundingBox", "time/assignParticles",
+    #                "time/pseudoParticle"] #, "time/loadBalancing"]
+
+
+    relevantKeys = [#"time/loadBalancing",
+                    "time/reset",
+                    "time/removeParticles",
+                    "time/boundingBox",
+                    "time/assignParticles",
+                    "time/tree",
+                    "time/pseudoParticle",
+                    "time/gravity", 
+                    "time/sph"]
+                    #"/time/IO"]
 
     offset = 0
+
     for i_key, key in enumerate(relevantKeys):
+        # if performance[key].get_data_average() != 0.0:
         stack.append(performance[key].get_data_average())
         bar.append(mean(performance[key].average()))
         #offset += bar[i_key]
         labels.append(performance[key].name)
 
+
     #np_stack = np.vstack(stack)
+
+    print("stack: {}".format(stack))
 
     fig, ax = plt.subplots()
     ax.stackplot([i for i in range(len(stack[0]))], stack, labels=labels)
+    ax.scatter([i for i in range(len(rhsElapsedPerStep))], rhsElapsedPerStep)
     ax.legend(loc='best')
-
     plt.show()
 
+    """
     fig, ax = plt.subplots()
     for i_key, key in enumerate(relevantKeys):
+        print("key: {}".format(key))
+        # if bar[i_key] != 0.0:
         ax.bar([0], bar[i_key], 0.5, bottom=offset, label=labels[i_key]) #, color='b')
         offset += bar[i_key]
     ax.legend(loc='best')
     plt.show()
+    """
 
     rhsElapsed = performance["time/rhsElapsed"].get_data_average()
     print("average rhs elapsed: {}".format(mean(rhsElapsed)))
